e1732a63dd9dc6d82bdb47b6ccd88925394cb3b5
Aryan Khaire <aryan.khaire001@gmail.com>
2025-12-15 23:01:06 +0530

    Background added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
-import {Routes,Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar.js";
 import LandingPage from './components/LandingPage/LandingPage.js';
 import Register from './components/Register/Register.js';
 import Login from './components/Login/Login.js';
+import Squares from './components/Animated Components/squareGrid.js';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+
+      <Squares
+        speed={0.5}
+        squareSize={40}
+        direction='diagonal'
+        borderColor='#dddd'
+        hoverFillColor='#ffff'
+      />
 
       <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </div>
   );
