7af988362f3de3de1315f48a5ce4b2f4518887ce
Aryan Khaire <aryan.khaire001@gmail.com>
2025-12-30 23:08:01 +0530

    Minor Bugs

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || 5000;
 const PORT = process.env.PORT;
 
 //Routes
