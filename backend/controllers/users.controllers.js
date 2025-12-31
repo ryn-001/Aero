46484fccea0d663368c210e8388f210e29759dec
Aryan Khaire <aryan.khaire001@gmail.com>
2025-12-31 17:52:29 +0530

    Login issue

--- backend/controllers/users.controllers.js
+++ backend/controllers/users.controllers.js
@@ -53,7 +53,7 @@ const loginUser = async (req,res) => {
         const {email,password} = req.body;
         if(!email || !password) return res.status(400).json({message: 'Email and password are required'});
 
-        const user = await User.findOne({email});
+        const user = await User.findOne({email}).select('+password');
         if(!user) return res.status(401).json({message: 'Invalid Email'});
         
         const isMatch = await bcrypt.compare(password,user.password);
@@ -78,7 +78,7 @@ const loginUser = async (req,res) => {
         });
 
     }catch(e){
-        console.log(e);
+        console.log(e.message);
         return res.status(500).json({ error: e.message });
     }
 }
